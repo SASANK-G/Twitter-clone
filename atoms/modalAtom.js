ffb892b9e50408d4026f9e53b65074c29bd0d46b
SASANK-G <gsbnsrsasank@gmail.com>
2021-12-11 14:15:40 +0530

    Feed-post-bottom

---
+++ atoms/modalAtom.js
@@ -0,0 +1,20 @@
+
+import { atom } from "recoil";
+
+//This is how we write atom using useState
+//useState cannot be used globally
+//const [modalState, setModalState] = useState(false)
+
+
+//atom can be used globally anywhere
+export const modalState = atom({
+    key:"modalState",
+    default:false,
+
+});
+
+export const postIdState = atom({
+    key:"modalState",
+    default:"",
+
+});
